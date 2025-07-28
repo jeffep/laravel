ee0fcc9007b87ee39e892201f09e4d797e572fdc
jeffrey <jeffrey@jarvis.jlane.com>
2025-07-27 19:18:50 -0600

    Strange error with Migration which was causing laravel to crash

--- app/Console/Commands/MigrateAllLegacyData.php
+++ app/Console/Commands/MigrateAllLegacyData.php
@@ -1,8 +1,34 @@
 <?php
 
-public function handle() {
-    $this->call('sensor:migrate-data');
-    $this->call('user:migrate-data');
-    $this->call('shelly:migrate-data');
-}
+namespace App\Console\Commands;
 
+use Illuminate\Console\Command;
+
+class MigrateAllLegacyData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:legacy-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate all legacy data from sensor, user, and shelly sources';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->call('sensor:migrate-data');
+        $this->call('user:migrate-data');
+        $this->call('shelly:migrate-data');
+    }
+}
